daf4e52b0c116697e303a5bffaff6db060df9e33
Sohaghori839 <sohaghori839@gmail.com>
2023-06-09 20:53:41 +0500

    assignment complete

--- chapter17-20/app.js
+++ chapter17-20/app.js
@@ -12,3 +12,89 @@ console.table(multiarr)*/
 // ------------------- task 4---------------
 
 
+// ------------------ task 5 ---------------------------
+// let fruit =["Apple", "Banana","Mango","Orange","Strawberry"];
+// for (let i = 0; i < fruit.length; i++) {
+//    document.write(fruit[i] + "<br>")
+// }
+
+// ---------------------------- task 6 ---------------------------
+// ==== counting =====
+// for (let i = 1; i<=15; i++) {
+// document.write(i + '<br>')
+// }
+
+// ==== reverse ====
+// for (let i = 15; i >= 1; i--) {
+//   document.write(i + "<br>")
+// }
+
+// ==== even =========
+/*for (let i = 0; i <= 20; i++) {
+    if (i%2=== 0) {
+      document.write(i + "<br>")
+   }
+}*/
+
+//==========odd ===============
+// for (let i = 0; i <= 20; i++) {
+//    if (i%2 !== 0) {
+//      document.write(i + "<br>")
+//    }
+// }
+
+// ============ series ==========
+// for (let i = 1; i <= 20; i++) {
+//    if (i%2 === 0) {
+//      document.write(i + "K" +"<br>")
+// }}
+
+// task 7
+// var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+// var userInput = prompt("Enter an item to search in bakery:");
+
+// var check = false;
+// for (var i = 0; i < A.length; i++) {
+//   if (A[i] === userInput) {
+//     found = true;
+//     break;
+//   }
+// }
+
+// if (check) {
+//   document.write(userInput + "   available in bakery");
+// } else {
+//   document.write(userInput +"    not available in bakery.");
+// }
+
+// task 8
+// var A = [24, 53, 78, 91, 12];
+// var largestNumber = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+//   if (A[i] > largestNumber) {
+//     largestNumber = A[i];
+//   }
+// }
+// console.log("The largest number is: " + largestNumber);
+
+// task 9
+// var A = [24, 53, 78, 91, 12];
+// var smallestNumber = A[0];
+
+// for (var i = 1; i < A.length; i++) {
+//   if (A[i] < smallestNumber) {
+//     smallestNumber = A[i];
+//   }
+// }
+// console.log("The smallest number is: " + smallestNumber);
+
+// task 10
+for (let i = 5; i<=100; i++) {
+   // document.write(i)
+   if (i % 5 === 0) {
+      document.write(i + "<br>")
+   }
+}
+
+
