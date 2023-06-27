d3089a5bafd63fdd118e8bf836d47d6c8e1f3d11
Sohaghori839 <sohaghori839@gmail.com>
2023-06-28 02:25:03 +0500

    more chpt

---
+++ chapter31-34/app.js
@@ -0,0 +1,67 @@
+// Task 1
+/* let current_date = new Date();
+document.write(current_date) */
+
+// Task 2
+/* let month = ["january","Febuary","March","April","May","June","July","Augest","September","October","November","December"];
+let d = new Date();
+let current_month = d.getMonth();
+let month_get = month[current_month]
+alert("current month: "+ month_get)
+ */
+
+// Task 3
+/* let day = ["Mon","Tue","Wed","Thur","Fri","Sat","Sun"];
+let d = new Date();
+let current_day = d.getDay();
+let today_date =day[current_day];
+alert("Today day is: "+ today_date) */
+
+// Task 4
+/* let d = new Date();
+d.toString;
+let e = d.toString();
+let f = e.slice(0,3);
+if (f === "Sat") {
+    document.write("Today is funny day");
+}
+else if (f === "Sun"){
+    document.write("Today is funny day");
+}
+else if (f === "Mon"){
+    document.write("Today Monday");
+}
+else if (f === "Tue"){
+    document.write("Today Tuesday");
+}
+else if (f === "wed"){
+    document.write("Today wednesday");
+}
+else if (f === "Thu"){
+    document.write("Today Thursday");
+}
+else if (f === "Fri"){
+    document.write("Today Friday");
+}
+else{
+    document.write("Enter your day")
+} */
+var currentDate = new Date();
+currentDate.toString;
+var b = currentDate.toString();
+var c = b.slice(0,3);
+if (c === "Sat"){
+    document.write("It's Fun Day")
+}else if(c === "Sun"){
+    document.write("It's Fun Day")
+}else if(c === "Mon"){
+    document.write()
+}else if(c === "Tue"){
+    document.write()
+}else if(c === "Wed"){
+    document.write()
+}else if(c === "Thr"){
+    document.write()
+}else if(c === "Fri"){
+    document.write()
+}
