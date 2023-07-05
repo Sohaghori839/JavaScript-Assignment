4281cc2957cb66f845c9e0d3763dafeca92de481
Sohaghori839 <sohaghori839@gmail.com>
2023-07-05 23:31:45 +0500

    code

--- chapter 26-30/app.js
+++ chapter 26-30/app.js
@@ -1,5 +1,56 @@
-let num = +prompt("Enter the possitive integer");
+// Task 1
+/* let num = +prompt("Enter the possitive integer");
 let roundNum= Math.round(num);
 let floorNum =  Math.floor(num);
 let ceilNum =  Math.ceil(num);
 document.write("number: " + num + "<br>"+ "round off value: "+ roundNum + "<br>" + "Floor value: " + floorNum  + "<br>"+ "Ceil value: "+ ceilNum);
+ */
+
+// Task 2
+/* var number = prompt("Enter a negative floating-point number:");
+number = parseFloat(number);
+if (isNaN(number) || number >= 0) {
+  document.write("Invalid input. Please enter a negative floating-point number.");
+} else {
+  document.write("Number: " + number + "<br>");
+
+  // Round off value
+  var roundOffValue = Math.round(number);
+  document.write("Round off value: " + roundOffValue + "<br>");
+
+  // Floor value
+  var floorValue = Math.floor(number);
+  document.write("Floor value: " + floorValue + "<br>");
+
+  // Ceil value
+  var ceilValue = Math.ceil(number);
+  document.write("Ceil value: " + ceilValue + "<br>");
+}
+ */
+// The math.abs() function is used to return the absolute value in JavaScript. It negates the native sign of a number and returns the relevant positive value.
+/* let num = +prompt("Enter the negitive num");
+let absNum = Math.abs(num);
+document.write(absNum) */
+
+// Task 4
+/* var bigDecimal = Math.random();
+var improvedNum = (bigDecimal * 6) + 1;
+var numberOfStars = Math.floor(improvedNum);
+ document.write("random dice value: "+numberOfStars)
+ */
+
+// Task 5
+/* let num = Math.random();
+if (num < 0.5) {
+    document.write("Random coins value is: Head")
+}
+else{
+    document.write("Random coins value is: Tail")
+} */
+
+// Task 6
+/* let num = Math.floor(Math.random()*100)+1;
+document.write("random num btw 1 and 100: "+num);
+ */
+
+// Task 7
