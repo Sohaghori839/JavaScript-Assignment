948600d0c046a44d9d3db83f6b64b443298aceec
Sohaghori839 <sohaghori839@gmail.com>
2023-07-05 18:58:46 +0500

    chpt 31-34

--- chapter31-34/app.js
+++ chapter31-34/app.js
@@ -18,50 +18,90 @@ let today_date =day[current_day];
 alert("Today day is: "+ today_date) */
 
 // Task 4
-/* let d = new Date();
-d.toString;
-let e = d.toString();
-let f = e.slice(0,3);
-if (f === "Sat") {
-    document.write("Today is funny day");
-}
-else if (f === "Sun"){
-    document.write("Today is funny day");
-}
-else if (f === "Mon"){
-    document.write("Today Monday");
-}
-else if (f === "Tue"){
-    document.write("Today Tuesday");
-}
-else if (f === "wed"){
-    document.write("Today wednesday");
-}
-else if (f === "Thu"){
-    document.write("Today Thursday");
-}
-else if (f === "Fri"){
-    document.write("Today Friday");
+/* let date =  new Date();
+let day = date.getDay();
+let days = ["Monday","Tuesday","Wedesday","Thursday","Friday","Saturday","Sunday"];
+if (days === "Saturday" || days === "Sunday") {
+    document.write("Today is fun day")
 }
 else{
-    document.write("Enter your day")
+    document.write("Today not fun day")
 } */
-var currentDate = new Date();
-currentDate.toString;
-var b = currentDate.toString();
-var c = b.slice(0,3);
-if (c === "Sat"){
-    document.write("It's Fun Day")
-}else if(c === "Sun"){
-    document.write("It's Fun Day")
-}else if(c === "Mon"){
-    document.write()
-}else if(c === "Tue"){
-    document.write()
-}else if(c === "Wed"){
-    document.write()
-}else if(c === "Thr"){
-    document.write()
-}else if(c === "Fri"){
-    document.write()
+
+// Task 5
+/* let date =  new Date();
+let day = date.getDay();
+if (day<16) {
+    document.write("First fifteen days of the month");
 }
+else{
+    ocument.write("Last days of the monnth");
+} */
+
+// Task 6
+/* let date =  new Date();
+let millsec = date.getTime()
+document.write("Current date: "+ date + "<br>" + "ELasped milliseconds since January 1,1970: "+ millsec + "</br>");
+document.write("ELasped mints since January 1,1970: " + (millsec/60));
+*/
+
+// Task 7
+/* let date = new Date();
+let hour = date.getHours();
+if (hour < 12) {
+    alert("It's AM")
+}
+else{
+    alert("It's PM")
+}
+ */
+
+// Task 8
+/* let date = new Date();
+let laterdate = new Date('9 nov 2025');
+document.write(laterdate); */
+
+// Task 9
+/* var startRamadan = new Date("May 18, 2025");
+var currentDate = new Date();
+var timeDiff = Math.abs(currentDate.getTime() - startRamadan.getTime());
+var daysPassed = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+
+alert("Number of days passed since 1st Ramadan: " + daysPassed);
+ */
+
+// Task  11
+/* let date =  new Date();
+let hour =  date.getHours();
+date.setHours(hour+1);
+document.write("Current date: "+ Date() + " <br>")
+document.write("Date after 1 hour: "+ date) */
+
+// Task  12
+/* var currentDate = new Date();
+currentDate.setFullYear(currentDate.getFullYear() - 100);
+var formattedDate = currentDate.toDateString();
+alert("100 years back from today is: " + formattedDate);
+ */
+
+// Task 13
+/* let userAge =  +prompt("Enter your age");
+let date = new Date();
+date.setFullYear(date.getFullYear()-userAge);
+document.write("your age is: " + userAge + " <br>");
+let formattedDate = date.toDateString();
+document.write("Your birth year is: "  + formattedDate ) */
+
+// Task 14
+
+/* let customerName =  prompt("Enter your name");
+let date = new Date();
+let month =  date.getMonth();
+let unit =  410;
+let perunit = 16;
+let netAmount =  unit*perunit;
+let lateCharges =  350;
+let grossAmt = netAmount+lateCharges;
+document.write("Customer name: "+ customerName + "<br>"+ "Month: " + month + "<br>" + "Number of units: " + unit + "<br>"+ "Charges of per unit: "+ perunit
++ "<br>" + "Net Amount Payable (within Due Date): " + netAmount + "Late payment surcharge: "+ lateCharges + 
+"Gross Amount Payable (after Due Date): "+ grossAmt); */
